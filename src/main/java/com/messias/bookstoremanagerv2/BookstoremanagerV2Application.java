d0a8805835d73bf059a2ed1fa8276e77ec7af6f8
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-07-02 15:41:20 -0300

    Criado bookstoremanager V2

---
+++ src/main/java/com/messias/bookstoremanagerv2/BookstoremanagerV2Application.java
@@ -0,0 +1,13 @@
+package com.messias.bookstoremanagerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoremanagerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/messias/bookstoremanagerv2/BookstoremanagerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messias.bookstoremanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
