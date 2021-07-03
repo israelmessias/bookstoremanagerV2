638797a8f1c731c22373ce2b816648c389b81dbc
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-07-03 14:26:33 -0300

    Controller Book

--- src/main/java/com/messias/bookstoremanagerv2/controller/BookController.java
+++ src/main/java/com/messias/bookstoremanagerv2/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.messias.bookstoremanagerv2.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v2/books")
 public class BookController {
-
+    @ApiOperation(value = "Return an example hello World")
+    @ApiResponses(value={
+            @ApiResponse(code=200, message = "Sucess method return")
+    })
     @GetMapping
     public String mensage(){
         return "Hello World";
