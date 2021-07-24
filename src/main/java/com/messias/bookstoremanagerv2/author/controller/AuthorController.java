060264149e3ebeec30de863d1c85b623dc605f35
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-07-24 00:38:56 -0300

    Criado metodo POST

--- src/main/java/com/messias/bookstoremanagerv2/author/controller/AuthorController.java
+++ src/main/java/com/messias/bookstoremanagerv2/author/controller/AuthorController.java
@@ -1,20 +1,35 @@
 package com.messias.bookstoremanagerv2.author.controller;
 
+import javax.validation.Valid;
+
+import com.messias.bookstoremanagerv2.author.dto.AuthorDTO;
 import com.messias.bookstoremanagerv2.author.service.AuthorService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/v2/authors")
 public class AuthorController {
+    private AuthorService authorService;
 
     @Autowired
-    public AuthorController(AuthorService authorService) {
+    public AuthorController(AuthorService authorService) 
+    {
         this.authorService = authorService;
     }
 
-    private AuthorService authorService;
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthorDTO create(@RequestBody @Valid AuthorDTO authorDTO) 
+    {
+        return authorService.create(authorDTO);
+    }
+    
 }
